c2e7d8c3e68e1895277e9df1cb8f55b93fd4e40f
CedricJoder <cedric.joder@gmail.com>
2025-04-08 23:09:13 +0200

    Update api and merge Dante module

--- src/variables.js
+++ src/variables.js
@@ -6,6 +6,7 @@ module.exports = {
 
 		variables.push({ variableId: 'channel_count', name: 'Channel Count' });
 		variables.push({variableId: 'devices', name: 'Dante Devices'});
+		variables.push({variableId: 'devices_channels', name: 'Devices Channels Count'});
 			
 		self.setVariableDefinitions(variables);
 	},
@@ -16,7 +17,8 @@ module.exports = {
 		try {
 			self.setVariableValues({
 				'channel_count': self.DEVICEINFO.channelCount,
-				'devices': self.Dante.devices
+				'devices': self.devices,
+				'devices_channels': self.devices_channels
 			});
 		}
 		catch(error) {
